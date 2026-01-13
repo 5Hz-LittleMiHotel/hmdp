5daceba41b0caa3a8ca97f368a67cc8e164d5148
5Hz <shz20030726@163.com>
2026-01-13 19:59:21 +0800

    优惠券秒杀

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -3,13 +3,15 @@ package com.hmdp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 
 @MapperScan("com.hmdp.mapper")
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true) // 引入aspectjweaver依赖, 然后这里使用注解来暴露动态代理对象
 public class HmDianPingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HmDianPingApplication.class, args);
     }
-
 }
